b1770488ac516261204c7fbd1619f0f0629f2847
kingskiss <75832671@qq.com>
2017-03-30 11:49:03 +0800

    变更项目名称为英文

---
+++ NonlinearEditSystem/Forms/CaptureForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+
+namespace 主界面.窗体
+{
+    public partial class CaptureForm : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public CaptureForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
