39eb8efa591fee088a2ec8dfd78120bf4866a145
kingskiss <75832671@qq.com>
2017-03-30 11:08:32 +0800

    添加新建工程界面

---
+++ 主界面/窗体/CreateProjectSetForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+
+namespace 主界面.窗体
+{
+    public partial class CreateProjectSetForm : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public CreateProjectSetForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonX_PreferSetCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
